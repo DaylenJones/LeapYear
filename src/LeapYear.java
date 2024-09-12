212a8c536f03587d1b308baaeb05631982df63da
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-12 12:55:45 -0400

    update

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LeapYear {
+
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    private static boolean isLeapYear(int year) {
+
+        if(year < 1 || year > 9999){
+            return false;
+        }
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return true;
+            }
+        }else{
+            return false;
+        }
+
+    }
+
+
 }
